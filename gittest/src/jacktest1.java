d6fa42865a342307426e42e979185c3b4432ada5
jack <2710624455@qq.com>
2024-01-04 21:21:59 +0800

    change update

--- gittest/src/jacktest1.java
+++ gittest/src/jacktest1.java
@@ -3,4 +3,22 @@
  * @create 2024-01-2024/1/4-20:55
  */
 public class jacktest1 {
+    private int  jackid;
+    public String jackname;
+
+    public int getJackid() {
+        return jackid;
+    }
+
+    public void setJackid(int jackid) {
+        this.jackid = jackid;
+    }
+
+    public String getJackname() {
+        return jackname;
+    }
+
+    public void setJackname(String jackname) {
+        this.jackname = jackname;
+    }
 }
